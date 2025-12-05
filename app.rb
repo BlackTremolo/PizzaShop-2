5f7e1d714affe45299e2c2d0b764097f705218f1
BlackTremolo <mihsee436@gmail.com>
2025-12-05 22:19:25 +0800

    создание класса Product и подключение к БД

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
+set :database, {adapter: "sqlite3", database: "pizzashop-2.db"}
+
+class Product <ActiveRecord::Base
+
+end
+
 get '/' do
 	erb :index
 end
