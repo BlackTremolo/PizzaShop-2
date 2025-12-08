414bea63b189eb11a1125af8c1c1d084f4423820
BlackTremolo <mihsee436@gmail.com>
2025-12-08 16:29:03 +0800

    Функция получения  и передачи списка продуктов в корзину

--- public/app.js
+++ public/app.js
@@ -35,4 +35,20 @@ function cart_get_products()
 	return cnt
 }
 
+function cart_get_orders()
+{
+	var orders= '';
+	for (var i = 0; i < window.localStorage.length; i++) 
+	{
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+
+		if(key.indexOf('product_') == 0)
+			{
+				orders = orders + key + '=' + value + ',';
+			}
+	}
+	return orders
+}
+
 
