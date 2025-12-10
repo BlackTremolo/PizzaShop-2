9b796cf8f11ec782727163998ba44a6c5bf424d4
BlackTremolo <mihsee436@gmail.com>
2025-12-10 22:53:24 +0800

    Создание таблицы с заказами

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_05_145931) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_10_143323) do
+  create_table "orders", force: :cascade do |t|
+    t.text "orders_input"
+    t.text "name"
+    t.text "phone"
+    t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "discription"
